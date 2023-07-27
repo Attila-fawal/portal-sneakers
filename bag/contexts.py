b28ae4c6e46acb96f07de78b3150cc7bcfdeb8a5
Attila fawal <attila_faweli@hotmail.com>
2023-07-27 13:34:19 +0000

    aded error Handler to quantity and massages

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from products.models import Product, Size
 
 def bag_contents(request):
 
@@ -10,15 +10,23 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    if not isinstance(bag, dict):
+        return {"error": "Bag is not a dictionary"}
+
+    for item_id, size_and_quantity in bag.items():
+        if not isinstance(size_and_quantity, dict):
+            continue
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+        for size_id, quantity in size_and_quantity.items():
+            size = get_object_or_404(Size, pk=size_id)  # get the Size instance
+            total += quantity * product.price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': quantity,
+                'product': product,
+                'size': size,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -26,9 +34,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -1,25 +1,44 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product, ProductSize
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
 
+    """ Add a quantity of the specified product to the shopping bag """
+    product = get_object_or_404(Product, pk=item_id)
+    size_id = str(request.POST.get('size'))  # convert to string here instead of int
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    # Check if the quantity is less than 1
+    if quantity < 1:
+        messages.error(request, 'Quantity must be at least 1.')
+        return redirect(redirect_url)
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    # Retrieve the ProductSize instance for this product and size
+    product_size = ProductSize.objects.get(product=product, size__id=int(size_id))  # convert to int here for DB query
+
+    # Check if the requested quantity exceeds the available inventory
+    if quantity > product_size.quantity:
+        messages.error(request, f'Sorry, only {product_size.quantity} items are available.')
+    else: 
+        # Ensure we have a dictionary for this item in the bag
+        if item_id not in bag:
+            bag[item_id] = {}
+
+        # Check if we already have the product size in the bag
+        if size_id in bag[item_id]:
+            bag[item_id][size_id] += quantity
+            messages.success(request, f'Updated size {product_size.size} quantity of {product.name} to {bag[item_id][size_id]}')
+        else:
+            bag[item_id][size_id] = quantity
+            messages.success(request, f'Added size {product_size.size} of {product.name} to your bag')
+
+        request.session['bag'] = bag
+
     return redirect(redirect_url)
-    
