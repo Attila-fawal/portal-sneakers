d142f2c72838db944f48f9638548af6e2cd09539
Attila fawal <attila_faweli@hotmail.com>
2023-08-05 07:24:49 +0000

    added footer , updated index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from products.models import Product  
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page with new arrival items """
 
-    return render(request, 'home/index.html')
+    # Fetch the items that belong to the category with id 4
+    new_arrival_items = Product.objects.filter(category_id=4)
+
+    # Pass the items to the template
+    context = {
+        'new_arrival_items': new_arrival_items,
+    }
+
+    return render(request, 'home/index.html', context)
